ca6334dfb480e2300f853c3e0c22c707cd20da3c
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-16 19:41:05 +0300

    Add customer list

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTicariOtomasyon.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.Carilers.ToList();
+            return View(values);
+        }
+    }
+}
