c7eacd7e7be80147a66cb162bbb479f60e50d19f
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-17 21:29:47 +0300

    Add Personel Controller and Index view

---
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTicariOtomasyon.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.Personels.ToList();
+            return View(values);
+        }
+    }
+}
