0f35f1001477515c37b11908dd7a9dd9c5045ca2
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-22 21:36:22 +0300

    List multiple tables in a view & IEnumerable

--- MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.DAL;
+using MvcOnlineTicariOtomasyon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,14 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class UrunDetayController : Controller
     {
         // GET: UrunDetay
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+            //var values = c.Uruns.Where(x => x.UrunID == 1).ToList();
+            cs.Deger1 = c.Uruns.Where(x => x.UrunID == 1).ToList();
+            cs.Deger2 = c.Detays.Where(y => y.DetayID == 1).ToList();
+            return View(cs);
         }
     }
 }
