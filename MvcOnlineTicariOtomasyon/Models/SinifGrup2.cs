2542531d959a80bef5fd838d12661e44f4086036
ilkaycanguder <ilkaycanguder@gmail.com>
2024-09-25 21:38:56 +0300

    Final review of the Partial section

--- MvcOnlineTicariOtomasyon/Models/SinifGrup2.cs
+++ MvcOnlineTicariOtomasyon/Models/SinifGrup2.cs
@@ -7,7 +7,7 @@ namespace MvcOnlineTicariOtomasyon.Models
 {
     public class SinifGrup2
     {
-        public int Departman { get; set; }
+        public string Departman { get; set; }
         public int Sayi { get; set; }
     }
 }
