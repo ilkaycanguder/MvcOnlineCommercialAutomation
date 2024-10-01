d570c45b1418e460af6852ff123c330fee115d23
ilkaycanguder <ilkaycanguder@gmail.com>
2024-10-01 21:26:02 +0300

    Login functionality added

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,28 @@
+﻿using MvcOnlineTicariOtomasyon.DAL;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var mail = (string)Session["CariMail"];
+            var values = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.m = mail;
+            return View(values);
+        }
+
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.CariID).FirstOrDefault();
+            var values = c.SatisHarekets.Where(x => x.CariID == id).ToList();
+            return View(values);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using MvcOnlineTicariOtomasyon.DAL;
+using MvcOnlineTicariOtomasyon.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler p)
+        {
+            var bilgiler = c.Carilers.FirstOrDefault(x => x.CariMail == p.CariMail && x.CariSifre == p.CariSifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"] = bilgiler.CariMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+    }
+}
