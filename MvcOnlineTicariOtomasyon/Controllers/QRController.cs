36639fc6509843275fec076ee8930229538ca220
ilkaycanguder <ilkaycanguder@gmail.com>
2024-10-05 21:38:04 +0300

    Implemented QR Code Operations

---
+++ MvcOnlineTicariOtomasyon/Controllers/QRController.cs
@@ -0,0 +1,34 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class QRController : Controller
+    {
+        // GET: QR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string kod)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+                QRCodeGenerator.QRCode qRCode = qRCodeGenerator.CreateQrCode(kod, QRCodeGenerator.ECCLevel.Q);
+                using (Bitmap image = qRCode.GetGraphic(10))
+                {
+                    image.Save(ms, ImageFormat.Png);
+                    ViewBag.qrCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return View();
+        }
+    }
+}
